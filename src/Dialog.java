46ebb69dc10f878167a723b59e723acda927f8ff
”IuliiaGolod” <juliette136@gmail.com>
2023-09-22 11:34:15 +0300

    Удалены лишние комментарии, разгружен метод main с помощью метода createFieldAndStatistics() class Dialog

--- src/Dialog.java
+++ src/Dialog.java
@@ -1,4 +1,13 @@
+import api.api;
+import map.Field;
+import organisms.*;
+import statistics.Statistics;
+
 import java.util.Scanner;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Dialog {
     public void dialog() {
@@ -11,5 +20,53 @@ public class Dialog {
                 "Эти значения помогут создать остров и определить его размеры.");
         System.out.println("После ввода этих значений, мы сможем создать остров и запустить процесс выживания животных и растений.\n" +
                 "Помните, что только сильнейшие смогут преуспеть на этом острове. Удачи!");
+    }
+    public void createFieldAndStatistics() {
+        Field field = new Field();
+        for (int i = 0; i < 5; i++) {
+            field.add(new Plant());
+        }
+        for (int i = 0; i < 2; i++) {
+            field.add(new Sheep());
+            field.add(new Rabbit());
+            field.add(new Boar());
+            field.add(new Bison());
+            field.add(new Caterpillar());
+            field.add(new Deer());
+            field.add(new Duck());
+            field.add(new Goat());
+            field.add(new Horse());
+            field.add(new Mouse());
+        }
+        for (int i = 0; i < 5; i++) {
+            field.add(new Wolf());
+            field.add(new Fox());
+            field.add(new Eagle());
+            field.add(new Bear());
+            field.add(new Boa());
+        }
+
+        Statistics statistics = new Statistics(field);
+
+        System.out.println("Start:");
+        field.show();
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Future<?> animalLifecycleFuture = executor.submit(() -> api.animalLifecycle(field, statistics));
+        Future<?> plantGrowthFuture = executor.submit(() -> api.plantGrowth(field, statistics));
+
+        executor.shutdown();
+
+        try {
+            animalLifecycleFuture.get();
+            plantGrowthFuture.get();
+            System.out.println();
+            System.out.println("Result:");
+            field.show();
+        } catch (CancellationException e) {
+            System.err.println("Task was cancelled: " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
